9feaac2da1799b59afe73d1b561fa698fd92072d
maximaCoder <62437851+maximaCoder@users.noreply.github.com>
2021-04-18 16:00:28 +0000

    unfinished seo stuff

--- pages/_document.js
+++ pages/_document.js
@@ -6,17 +6,42 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="title" content="Amine Tayani - full stack developer" />
+          <meta
+            name="keywords"
+            content="amine tayani , amine , tayani , full stack developer , morocco"
+          />
           <meta
             name="description"
             content="Amine Tayani is  a full stack developer, passionate about web development especially Javascript (React, NodeJS, MERN stack development...), I have built many projects using laravel, Django, and a lot of responsive and fully functional websites."
           />
+          <meta name="robots" content="index, follow" />
+          <meta name="language" content="English" />
+          <meta name="author" content="Amine Tayani" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://aminet.vercel.app/" />
+          <meta property="og:title" content="Amine Tayani - full stack developer" />
+          <meta
+            property="og:description"
+            content="Amine Tayani is  a full stack developer, passionate about web development especially Javascript (React, NodeJS, MERN stack development...), I have built many projects using laravel, Django, and a lot of responsive and fully functional websites."
+          />
+          <meta property="og:image" content="https://aminet.vercel.app/assets/screen.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://aminet.vercel.app/" />
+          <meta property="twitter:title" content="Amine Tayani - full stack developer" />
+          <meta
+            property="twitter:description"
+            content="Amine Tayani is  a full stack developer, passionate about web development especially Javascript (React, NodeJS, MERN stack development...), I have built many projects using laravel, Django, and a lot of responsive and fully functional websites."
+          />
+          <meta property="twitter:image" content="https://aminet.vercel.app/assets/screen.png" />
           <meta name="msapplication-TileColor" content="#2b5797" />
           <meta name="theme-color" content="#ffffff" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+          <link rel="manifest" href="/assets/site.webmanifest" />
+          <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5" />
 
           <link
             rel="preload"
