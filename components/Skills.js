9b5c4ceb0fd7a55c05f0436f7057d2ffe10f4ed8
Amine <62437851+amine-tayani@users.noreply.github.com>
2022-08-24 15:23:50 +0100

    commit

--- components/Skills.js
+++ components/Skills.js
@@ -44,18 +44,7 @@ const Skills = () => {
           {skills.map((skill, index) => {
             return (
               <li key={index} className="flex items-center space-x-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-purpley"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                ▹
                 <span className="text-gray-200 text-sm">{skill}</span>
               </li>
             );
