0c5d51f422da276da3818101e465d5155531ccfb
Amine <62437851+amine-tayani@users.noreply.github.com>
2023-09-07 19:42:14 +0100

    Add Google analytics script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "../../globals.css";
 import { Inter } from "next/font/google";
+import  Script from "next/script"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,6 +16,19 @@ export default function RootLayout({
   return (
     <html>
       <body className={inter.className}>{children}</body>
+      <Script
+    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_APP_GA_MEASUREMENT_ID}`}
+      />
+    <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_APP_GA_MEASUREMENT_ID}');
+
+        `}
+    </Script>
     </html>
   );
 }
