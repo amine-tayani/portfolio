d78435272e43571932d799d539666f00d8eac91b
Amine Tayani <amine.tayanipro@gmail.com>
2024-03-13 15:21:06 +0000

    chore: Update experiences and projects sections to latest version

--- src/components/v2/ExperienceTabPanel.tsx
+++ src/components/v2/ExperienceTabPanel.tsx
@@ -26,6 +26,86 @@ const ExperienceTabPanel: React.FC = () => {
                   className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
                   aria-label=" Mar - Sep 2021"
                 >
+                  Jan 2024
+                </header>
+                <div className="z-10 sm:col-span-6">
+                  <h3 className="font-medium leading-snug text-slate-200">
+                    <div>
+                      <Link
+                        className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                        href="https://www.mediaexperts.ma/"
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="Full Stack Developer - Media Experts"
+                      >
+                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                        <span>
+                          Full Stack Developer ·{" "}
+                          <span className="inline-block">
+                            Media Experts
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 ml-1 translate-y-px"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                clipRule="evenodd"
+                              ></path>
+                            </svg>
+                          </span>
+                        </span>
+                      </Link>
+                    </div>
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    Developed a website for Jeanne d{`'`}Arc International
+                    School Casablanca using Reactjs, implemented the Node.js
+                    backend for Bzioui Immobilier{`'`}s real estate website and
+                    dashboard for adding,Uploading Media Files, editing and
+                    deleting projects and blogs.
+                  </p>
+                  <ul
+                    className="mt-2 flex flex-wrap"
+                    aria-label="Technologies used"
+                  >
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        React
+                      </div>
+                    </li>
+
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        Tailwind CSS
+                      </div>
+                    </li>
+
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        JavaScript
+                      </div>
+                    </li>
+                    <li className="mr-1.5 mt-2">
+                      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                        TypeScript
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </Reveal>
+          </li>
+          <li className="mb-12">
+            <Reveal>
+              <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <header
+                  className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2"
+                  aria-label=" Mar - Sep 2021"
+                >
                   Feb 2021 - Jan 2022
                 </header>
                 <div className="z-10 sm:col-span-6">
@@ -67,10 +147,11 @@ const ExperienceTabPanel: React.FC = () => {
                     </div>
                   </h3>
                   <p className="mt-2 text-sm leading-normal">
-                    Developed interactive websites for clients using Next.js and React. catering to
-                    diverse domains, such as Cryptocurrency and Fitness. Collaborate with clients, Advocate developers,
-                    UI / UX Designers to transform creative concepts into production realities.
-
+                    Developed interactive websites for clients using Next.js and
+                    React. catering to diverse domains, such as Cryptocurrency
+                    and Fitness. Collaborate with clients, Advocate developers,
+                    UI / UX Designers to transform creative concepts into
+                    production realities.
                   </p>
                   <ul
                     className="mt-2 flex flex-wrap"
--- src/components/v2/ProjectTabPanel.tsx
+++ src/components/v2/ProjectTabPanel.tsx
@@ -26,6 +26,108 @@ const ProjectTabPanel: React.FC = () => {
                   <h3>
                     <Link
                       className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                      href="https://github.com/amine-tayani/trilingue"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label="Jeanne d'Arc International School website"
+                    >
+                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      <span>
+                        Jeanne d{`'`}Arc International School website{" "}
+                        <span className="inline-block">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </span>
+                    </Link>
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    I created a website for Jeanne D{`'`}Arc International
+                    Casablanca School using React and Typescript from a design
+                    template.
+                  </p>
+                </div>
+                <Image
+                  alt="Jeanne d'Arc International School website"
+                  width="200"
+                  height="48"
+                  className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                  style={{ background: "transparent" }}
+                  src={"/assets/cole-jeanne-darc.jpeg"}
+                />
+              </div>
+            </Reveal>
+          </li>
+          <li className="mb-12">
+            <Reveal>
+              <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="z-10 sm:order-2 sm:col-span-6">
+                  <h3>
+                    <Link
+                      className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                      href="https://github.com/amine-tayani/bzioui-api/"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label="Bzioui Project"
+                    >
+                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      <span>
+                        Bzioui Immobilier{" "}
+                        <span className="inline-block">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </span>
+                    </Link>
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    I built a full-stack real estate application. The backend
+                    (Express, TypeScript) uses MongoDB to store listings and
+                    lets authorized admins add, edit, and delete properties and
+                    blogs, File Upload with Cloudinary (single & multiple photos
+                    and video).
+                  </p>
+                </div>
+                <Image
+                  alt="Bzioui Immobilier Api"
+                  width="200"
+                  height="48"
+                  className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                  style={{ background: "transparent" }}
+                  src={"/assets/bzioui-immo.jpg"}
+                />
+              </div>
+            </Reveal>
+          </li>
+          <li className="mb-12">
+            <Reveal>
+              <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="z-10 sm:order-2 sm:col-span-6">
+                  <h3>
+                    <Link
+                      className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
                       href="https://amine-tayani.vercel.app/"
                       target="_blank"
                       rel="noreferrer"
