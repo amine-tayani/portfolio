2184a9092bb0b9c8627ee5d26c136757b1b4abdb
Amine Tayani <amine.tayanipro@gmail.com>
2024-03-25 11:41:23 +0000

    docs: Add two projects(Appeo + tspotfy-ui)

--- src/components/v2/ProjectTabPanel.tsx
+++ src/components/v2/ProjectTabPanel.tsx
@@ -26,6 +26,108 @@ const ProjectTabPanel: React.FC = () => {
                   <h3>
                     <Link
                       className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                      href="https://traceapp.vercel.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label="Trace Landing page"
+                    >
+                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      <span>
+                        Appeo
+                        <span className="inline-block">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </span>
+                    </Link>
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    Appeo is SaaS Landing Page for a web app that organizes and
+                    tracks your job applications with ease. This landing page
+                    includes a Compelling Headline, Clear Call-to-Action and a
+                    Minimalist Design.
+                  </p>
+                </div>
+                <Image
+                  alt="Trace App page"
+                  width="200"
+                  height="48"
+                  className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                  style={{ background: "transparent" }}
+                  src={"/assets/Appeo.png"}
+                />
+              </div>
+            </Reveal>
+          </li>
+          <li className="mb-12">
+            <Reveal>
+              <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="z-10 sm:order-2 sm:col-span-6">
+                  <h3>
+                    <Link
+                      className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                      href="https://spotifyui.vercel.app/"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label="Spotify UI"
+                    >
+                      <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                      <span>
+                        Spotify UI
+                        <span className="inline-block">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </span>
+                    </Link>
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    SpotifyUi mimics some of the features of Spotify
+                    {`'`}s interface, including the homepage, search page, and
+                    an engaging playlist detail page for exploring your music
+                    collections, .
+                  </p>
+                </div>
+                <Image
+                  alt="Spotify UI"
+                  width="200"
+                  height="48"
+                  className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                  style={{ background: "transparent" }}
+                  src={"/assets/Spotify-Home.png"}
+                />
+              </div>
+            </Reveal>
+          </li>
+          <li className="mb-12">
+            <Reveal>
+              <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="z-10 sm:order-2 sm:col-span-6">
+                  <h3>
+                    <Link
+                      className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
                       href="https://github.com/amine-tayani/trilingue"
                       target="_blank"
                       rel="noreferrer"
