70177e9cf803f75086eee9216eedcd30b12bb83f
Amine <62437851+amine-tayani@users.noreply.github.com>
2024-09-26 15:40:42 +0100

    move resume links

--- src/components/v2/HomePage.tsx
+++ src/components/v2/HomePage.tsx
@@ -27,6 +27,70 @@ const HomePage: React.FC = () => {
             web.
           </p>
           <NavigationLinks />
+
+          <div className="flex space-x-4 mt-12">
+            <Link
+              className="inline-flex items-center leading-tight text-slate-200 font-semibold group"
+              aria-label="Voir mon cv"
+              href="/assets/resume/Amine_CV_dernier.pdf"
+            >
+              <span>
+                <span className=" border-b border-transparent pb-px transition group-hover:border-teal-300">
+                  Voir mon
+                </span>
+                <span className="whitespace-nowrap">
+                  <span className="border-b border-transparent pb-px transition group-hover:border-teal-300">
+                    {" "}
+                    CV
+                  </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </span>
+            </Link>
+
+            <Link
+              className="inline-flex items-center leading-tight text-slate-200 font-semibold group"
+              aria-label="View my resume"
+              href="/assets/resume/RESUME_DEV_LTS.pdf"
+            >
+              <span>
+                <span className=" border-b border-transparent pb-px transition group-hover:border-teal-300">
+                  View my
+                </span>
+                <span className="whitespace-nowrap">
+                  <span className="border-b border-transparent pb-px transition group-hover:border-teal-300">
+                    {" "}
+                    Resume
+                  </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </span>
+              </span>
+            </Link>
+          </div>
         </div>
         <SocialMediaLinks />
       </header>
